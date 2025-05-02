d91702417503a9ef60e42704131ad579090d1e7f
Hadil Guenoua <hadilmayar100@gmail.com>
2025-05-02 15:32:34 +0100

    رفع مشروع مكتبة الكتب

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        title = request.form["title"]
+        if title:
+            new_book = Book(title=title)
+            db.session.add(new_book)
+            db.session.commit()
+            return redirect("/")
+    books = Book.query.all()
+    return render_template("index.html", books=books)
+
+@app.route("/books")
+def get_books():
+    books = Book.query.all()
+    books_list = [{"id": book.id, "title": book.title} for book in books]
+    return jsonify(books_list)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=5000)
